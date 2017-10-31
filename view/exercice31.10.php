ced6f5d17e59426a102953d5b8553f7694c69a41
Unknown <marreg@rocketmail.com>
2017-10-31 14:04:47 +0100

    Fonction suppression, ajout et debug marche

--- view/exercice31.10.php
+++ view/exercice31.10.php
@@ -1,40 +1,45 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>exercice du jour</title>
-</head>
-<body>
-<h1>
-	Exercice 01
-</h1>
-<!-- capacité utilisateur ajout mot dans un tableau et l'affiche. 
-ajout bouton supprime fruit. 
-ajoute bouton debug qui affiche contenu tableau -->
+	<head>
+		<title>exercice du jour</title>
+	</head>
+	<body>
+		<h1>
+		Exercice 01
+		</h1>
+		<!-- capacité utilisateur ajout mot dans un tableau et l'affiche.
+		ajout bouton supprime fruit.
+		ajoute bouton debug qui affiche contenu tableau -->
+		<input type="text" id="contenuUser">
+		<button onclick="fonctionFuitee();"> test </button>
+		<button onclick="fonctionNulle();"> Debug </button>
+		<p id="demo"></p>
+		<script>
+			tableFruit = [];
+			function fonctionFuitee(){
+				d = document.getElementById("contenuUser").value;
+				tableFruit.push(d);
+				e= tableFruit.indexOf(d);
+				document.getElementById("demo").innerHTML += d +"<button onclick='supprimeFruit("+e+")'>Supprimer</button> <br>";
+			}
 
-<input type="text" id="contenuUser">
-<button onclick="fonctionFuitee();"> test </button>
-<button onclick="fonctionNulle();"> Debug </button>
+			function supprimeFruit(s){
+					tableFruit.splice(s,1);
+					f = tableFruit.length;
+					x = 0;
+					document.getElementById("demo").innerHTML= "";
+					while (x != f+1) {
+						if (tableFruit[x] != null){
+							document.getElementById("demo").innerHTML = document.getElementById("demo").innerHTML + tableFruit[x]+"<button onclick='supprimeFruit("+x+")'>Supprimer</button> <br>";
+						
+						} 
+						x= x+1;
+					}
+				}
 
-<p id="demo"></p>
-
-<script>
-	tableFruit = [];
-	function fonctionFuitee(){
-		d = document.getElementById("contenuUser").value;
-		tableFruit.push(d);
-		e= tableFruit.indexOf(d);
-
-		document.getElementById("demo").innerHTML += d +"<button onclick='supprimeFruit(parseInt(e))'>Supprimer</button> <br>";
-	}
-	function supprimeFruit(s){
-			console.log(s);
-	}
-
-		function fonctionNulle(){
-			console.log(tableFruit);
-		}
-</script>
-
-
-</body>
+				function fonctionNulle(){
+					console.log(tableFruit);
+				}
+		</script>
+	</body>
 </html>
