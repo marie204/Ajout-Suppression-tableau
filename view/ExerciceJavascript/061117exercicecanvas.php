1c0921385cb0dc61645977b3310bfdfa9ca52e6f
Unknown <marreg@rocketmail.com>
2017-11-07 13:43:51 +0100

    Tenter de décomposé les choses

--- view/ExerciceJavascript/061117exercicecanvas.php
+++ view/ExerciceJavascript/061117exercicecanvas.php
@@ -1,28 +1,67 @@
+
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>canvas</title>
-	</head>
-	<body>
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<canvas id="myCanvas" width="200" height="200" style="border:1px solid #000000;">
-		</canvas>
-		<script>
-			dernierX = 0;
+<head>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+
+
+<script>
+				dernierX = 0;
+					dernierY = 0;
+
+$(document).ready(function(){
+    $('myCanvas').mousemove(function(){
+    	console.log('hoy');
+        var c = document.getElementById("myCanvas");
+		var ctx = c.getContext("2d");
+		ctx.moveTo(0,0);
+		ctx.lineTo(200,100);
+		ctx.stroke();
+    });
+});
+</script>
+</head>
+<body>
+
+<canvas id="myCanvas" width="200" height="100" style="border:1px solid #d3d3d3;">
+
+</body>
+</html>
+
+
+
+<!-- <script>
+				
+			document.getElementById("myCanvas").mousemove(fonctionDessin());
+
+
+			function fonctionDessin(){
+				var c = document.getElementById("myCanvas");
+				var ctx = c.getContext("2d");
+				ctx.moveTo(dernierX, dernierY);
+				ctx.lineTo(200,100);
+				ctx.stroke();
+			}
+
+
+
+		</script> -->
+
+		<!--<script>
+			/*dernierX = 0;
 			dernierY = 0;
 		document.getElementById("myCanvas").mousemove(fonctionDessin());
-			function fonctionDessin(){
+			function fonctionDessin(event){
 			console.log(event);
 			var c = document.getElementById("myCanvas");
 			var ctx = c.getContext("2d");
 			ctx.moveTo(dernierX, dernierY);
 			ctx.lineTo(event.clientX, event.clientY);
-			ctx.strokeStyle = '#000000';
+			ctx.strokeStyle = '#909090';
 			ctx.lineWidth = 10;
 			ctx.stroke();
 			dernierX = event.clientX;
 			dernierY = event.clientY;
-		}
-		</script>
-	</body>
-</html>
+		}*/</script>-->
+	
