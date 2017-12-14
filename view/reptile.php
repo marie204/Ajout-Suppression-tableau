eb7844fa9e9ead639071e393885d1785ae8df99b
Unknown <marreg@rocketmail.com>
2017-12-14 14:11:08 +0100

    Ebauche, début d'ajout pour que l'utilisateur puisse creer des pages
    Questionnement sur le fait de laisser ou non une BDD sur github.

--- view/reptile.php
+++ view/reptile.php
@@ -1,53 +1,33 @@
 
+<section id="corpsDocs">test</section>
 
 
-		<section>
-			<h1 id="serpents">Les serpents</h1>
-			<p>
-				<img src="./image/python-mini.jpg" class="miniaL" alt="image de serpent">
-				Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-			<p> <img src="./image/python-mini2.jpg" class="miniaR" alt="image de serpent">  Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-			proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-				t
-				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<br> <a href="./Classique.html#souris" target="iframeMain"> Pour plus d'information, voir fiche souris</a></p>
-				<h1> Les tortues </h1>
-			</section>
-			<section>
-				<p id="tortuePara">
-					<img src="./image/tortue-mini.jpg" class="miniaL" alt="image de tortue" >Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum. </p>
-					
-					<h1>Les dragons</h1>
-					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo <img src="./image/dragon-mini.jpg" class="miniaL" alt="image de dragon du komodo">
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-						proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-						tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-						quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-						consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-						cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				</section>
-				<!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.3/umd/popper.min.js" integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+<script>
+	//TODO Debugguer soucis du foreach! 
+	//TODO Terminer la possibilité pour l'utilisateur de créer des pages. 
+	$tableContenu = [];
+	$tableTitre = [];
+	$userName = [];
+	$prodDate = [];
+	$lastMod = [];
+	fonctionGeneration($tableContenu);
+	function fonctionGeneration(){
+		console.log('shoot');
+		$monCompte = fonctionCompte();
+		if ($monCompte > 0){
+			console.log('Marche');
+		}else{
+			document.getElementById('corpsDocs').innerHTML = "Rien à afficher ici pour le moment! Brandissez votre compte utilisateur pour changer ça!";
+		}
+	};
+	
+	function fonctionCompte($tableContenu)
+	{
+		a = 0;
+		foreach($tableContenu as $myKey){
+			$a += 1;
+		}
+	}
+	return $a;
+</script>
