1b12991ca2ea339621c5ded295cc6f4b42a27083
Unknown <marreg@rocketmail.com>
2018-01-08 10:20:30 +0100

    Dernière modifications de l'année 2017

--- view/reptile.php
+++ view/reptile.php
@@ -1,5 +1,5 @@
 
-<section id="corpsDocs">test</section>
+<section id="corpsDocs"><h5>Bienvenue sur notre section reptilienne, bonne lecture!</h5></section>
 <div id='testZone'></div>
 
 
@@ -15,21 +15,22 @@ echo '</script>';
 <script>
 	//TODO Debugguer soucis du foreach! 
 	//TODO Terminer la possibilité pour l'utilisateur de créer des pages. 
-	var $tableContenu = [];
-	var $tableTitre = [];
-	var $userName = [];
-	var $prodDate = [];
-	var $lastMod = [];
+	var tableContenu = [];
+	var tableTitre = [];
+	var userName = [];
+	var prodDate = [];
+	var lastMod = [];
 </script>
 		<?php remplirTableau(); ?>
 <script>
 	fonctionGeneration();
 	maSess == false ? null : fonctionCreateButton();
+	fonctionRemplir();
 	function fonctionGeneration(){
-		console.log('shoot');
-		$monCompte = $tableContenu.length;
-		if ($monCompte > 0){
-			
+		//console.log('shoot');
+		monCompte = tableContenu.length;
+		if (monCompte > 0){
+			document.getElementById('corpsDocs').innerHTML = 'test';
 		}else{
 			document.getElementById('corpsDocs').innerHTML = "Rien à afficher ici pour le moment! <br> ";
 			if (maSess== false) { document.getElementById('corpsDocs').innerHTML +=" Brandissez votre compte utilisateur pour changer ça!";}else{document.getElementById('corpsDocs').innerHTML += 'Montre l\'exemple et change moi ça, '+maSess+'!! Dirais-je si actuellement la fonction était terminée! <br>'; }
@@ -39,7 +40,7 @@ echo '</script>';
 	function fonctionCompte()
 	{
 		a = 0;
-		for (var i = 0;  i < $tableContenu.length; i++) {
+		for (var i = 0;  i < tableContenu.length; i++) {
 			a = a+1;
 		}
 	return a;
@@ -53,9 +54,12 @@ echo '</script>';
 		document.getElementById('corpsDocs').innerHTML += '<button id="boutonCreation" onClick="createForm();">monBouton</button>'
 	}
 	function fonctionDeleteButton(){
-		console.log('ok');
+		//console.log('ok');
 		document.getElementById('corpsDocs').innerHTML = "";
 		fonctionGeneration();
+	}
+	function fonctionRemplir(){
+		console.log(tableContenu);
 	}
 </script>
 <?php
@@ -63,8 +67,18 @@ function remplirTableau(){
 		$mesDocuments = scandir('./view/pagesreptile/');
 		for ($i=0; $i < count($mesDocuments) ; $i++) { 
 			if (strstr($mesDocuments[$i], '.txt')) {
-				echo '<script> $tableContenu.push("'.$mesDocuments[$i].'"); </script>';
-				echo $mesDocuments[$i].'<br>';
+				//echo $mesDocuments[$i];
+				$file0 = file('./view/pagesreptile/'.$mesDocuments[$i]);
+				/*$table = fopen('./view/pagesreptile/'.$mesDocuments[$i], 'r');
+				$file = fread($table, filesize('./view/pagesreptile/'.$mysqliesDocuments[$i]));*/
+				var_dump($file0);
+				//echo $file;
+				
+				
+				//echo '<script>console.log(tableContenu);</script>';
+			//$table = fclose($table);
+				//echo $mesDocuments[$i].'<br>';
+
 			}
 		}
 	}
